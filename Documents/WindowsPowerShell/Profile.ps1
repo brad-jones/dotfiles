80591dcf031b4018488168950a9110ab46215bf4
Brad Jones <brad@bjc.id.au>
2020-04-27 20:15:33 +1000

    chore: wip

--- Documents/WindowsPowerShell/Profile.ps1
+++ Documents/WindowsPowerShell/Profile.ps1
@@ -1,9 +1,9 @@
 . $env:USERPROFILE\Documents\WindowsPowerShell\utils.ps1;
 
 # Define our path as early as possible
-AddToPath -Path "$env:USERPROFILE\.local\sbin\bin";
 AddToPath -Path "$env:USERPROFILE\.local\bin";
 AddToPath -Path "$env:USERPROFILE\AppData\Roaming\Pub\Cache\bin";
+AddToPath -Path "$env:USERPROFILE\.local\sbin\bin";
 
 # Tell goenv where to install go, personally I prefer all my tools and
 # config to be hidden (ie: start with a dot) and all my actual data
